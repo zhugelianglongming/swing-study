551b928283e8ddb5bd76319842065793321fb84b
zhugelianglongming <zhugelianglongming@163.com>
2015-12-03 15:14:13 +0800

    Panel with Oval

---
+++ swing-study/src/swingstudy/ch04/OvalPanel.java
@@ -0,0 +1,55 @@
+package swingstudy.ch04;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class OvalPanel extends JPanel {
+
+	Color color;
+
+	public OvalPanel() {
+		this(Color.black);
+	}
+
+	public OvalPanel(Color color) {
+		this.color = color;
+	}
+
+	public void paintComponent(Graphics g) {
+		int width = getWidth();
+		int height = getHeight();
+		g.setColor(color);
+		g.drawOval(0, 0, width, height);
+	}
+
+	public static void main(String[] args) {
+
+		Runnable runner = new Runnable() {
+			public void run() {
+				JFrame frame = new JFrame("Oval Sample");
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+				frame.setLayout(new GridLayout(2, 2));
+
+				Color colors[] = { Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW };
+
+				for (int i = 0; i < 4; i++) {
+					OvalPanel panel = new OvalPanel(colors[i]);
+					frame.add(panel);
+				}
+
+				frame.setSize(300, 200);
+				frame.setVisible(true);
+			}
+		};
+
+		EventQueue.invokeLater(runner);
+	}
+
+}
