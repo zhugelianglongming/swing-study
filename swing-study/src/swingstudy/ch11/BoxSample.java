a0354d0e4b542f88fa46457efc46c0276f3ea2be
zhugelianglongming <zhugelianglongming@163.com>
2015-12-15 10:46:52 +0800

    Demo code for Box.

---
+++ swing-study/src/swingstudy/ch11/BoxSample.java
@@ -0,0 +1,46 @@
+package swingstudy.ch11;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class BoxSample {
+
+	public static void main(String[] args) {
+
+		Runnable runner = new Runnable() {
+			public void run() {
+				JFrame verticalFrame = new JFrame("Vertical");
+				verticalFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+				Box verticalBox = Box.createVerticalBox();
+				verticalBox.add(new JLabel("Top"));
+				verticalBox.add(new JTextField("Middle"));
+				verticalBox.add(new JButton("Bottom"));
+				verticalFrame.add(verticalBox, BorderLayout.CENTER);
+				verticalFrame.setSize(150, 150);
+				verticalFrame.setVisible(true);
+
+				JFrame horizontalFrame = new JFrame("Horizontal");
+				horizontalFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+				Box horizontalBox = Box.createHorizontalBox();
+				horizontalBox.add(new JLabel("Left"));
+				horizontalBox.add(new JTextField("Middle"));
+				horizontalBox.add(new JButton("Right"));
+				horizontalFrame.add(horizontalBox, BorderLayout.CENTER);
+				horizontalFrame.setSize(150, 150);
+				horizontalFrame.setVisible(true);
+				horizontalFrame.setLocation(150, 0);
+			}
+		};
+
+		EventQueue.invokeLater(runner);
+	}
+
+}
